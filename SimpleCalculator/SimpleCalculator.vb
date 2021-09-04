4fde6b5b326a8130fd1eb7dcbad10151780be8e1
Sebastian Soto <thefirstcoolone@gmail.com>
2021-09-03 20:44:15 -0600

    works

--- SimpleCalculator/SimpleCalculator.vb
+++ SimpleCalculator/SimpleCalculator.vb
@@ -6,9 +6,26 @@
 Option Strict On
 Option Explicit On
 Module SimpleCalculator
-
     Sub Main()
+        Dim optOne As String
+        Dim optTwo As String
+        Dim optPos As String
 
+        Console.WriteLine("Enter Number")
+        optOne = Console.ReadLine()
+        Console.WriteLine("Another one")
+        optTwo = Console.ReadLine()
+
+        Dim numOne As Integer = CInt(optOne)
+        Dim numTwo As Integer = CInt(optTwo)
+
+        Console.WriteLine("Product(*) or Sum(+)?")
+        optPos = Console.ReadLine()
+        If optPos = "+" Then
+            Console.WriteLine(numTwo + numOne)
+        ElseIf optPos = "*" Then
+            Console.WriteLine(numTwo * numOne)
+        End If
+        Console.ReadLine()
     End Sub
-
 End Module
