f3a4375f618ca8412ce1b25ba2a751bbcefe74f3
Sebastian Soto <thefirstcoolone@gmail.com>
2021-09-04 23:51:56 -0600

    add notes

--- SimpleCalculator/SimpleCalculator.vb
+++ SimpleCalculator/SimpleCalculator.vb
@@ -2,7 +2,7 @@
 'RCET0265
 'Fall 2021
 'SimpleCalculator
-'https://github.com/SebastianSotoMk4/SimpleCalculator.git
+'https://github.com/SebastianSotoMk4/SimpleCalculator.gitoo
 Option Strict On
 Option Explicit On
 Module SimpleCalculator
@@ -11,15 +11,15 @@ Module SimpleCalculator
         Dim optTwo As String
         Dim optPos As String
         Dim result As Integer
-
+        'Ask for numbers
         Console.WriteLine("Enter Number")
         optOne = Console.ReadLine()
         Console.WriteLine("Another one")
         optTwo = Console.ReadLine()
-
+        'convert string number to intiger
         Dim numOne As Integer = CInt(optOne)
         Dim numTwo As Integer = CInt(optTwo)
-
+        'Ask for operator and display answer
         Console.WriteLine("Product(*) or Sum(+)?")
         optPos = Console.ReadLine()
         If optPos = "+" Then
@@ -28,6 +28,7 @@ Module SimpleCalculator
             result = numTwo * numOne
         End If
         Console.WriteLine("Answer is " & result)
+        '
         Console.ReadLine()
     End Sub
 End Module
